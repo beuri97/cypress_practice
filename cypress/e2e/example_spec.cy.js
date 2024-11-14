6922327e1eb76f9e1fccbda1c947dcbb18a315cc
HanByeol Yang <hya62@uclive.ac.nz>
2024-11-14 13:51:56 +1300

    test: add practice test cases

--- cypress/e2e/example_spec.cy.js
+++ cypress/e2e/example_spec.cy.js
@@ -1,7 +1,26 @@
 describe('My First Test', () => {
   it('Visits the Kitchen Sink and checks for a title', () => {
     cy.visit('https://example.cypress.io');
-    cy.contains('type').click();
-    cy.url().should('include', '/commands/actions');
+    cy.contains('filter').click();
+    cy.url().should('include', '/commands/traversal');
+  });
+});
+
+describe('My Second Test', () => {
+  it("should filter id 'filter'", () => {
+    cy.visit('https://example.cypress.io/commands/traversal');
+    cy.get('h4').filter('#filter').should('contain', '.filter()');
+  });
+});
+
+describe('My Third Practice Test', () => {
+  it("Should be use eq to get eq", () => {
+    cy.visit('https://example.cypress.io');
+    cy.get(".home-list>li>ul>li").eq(6).should('contain', 'eq');
+  });
+  it("Try to be simpler", () => {
+      cy.visit('http://example.cypress.io');
+      cy.get(".home-list").find('a').contains('eq').click();
+      cy.url().should('contain', "/commands/traversal");
   });
 });
